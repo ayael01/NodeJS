02e876f70ed74c97502316d88bcbeb1d4f95ad39
reelooker <ayael01@gmail.com>
2022-11-07 17:06:34 +0200

    New file burgers

---
+++ Projects/RapidAPI/burgers.js
@@ -0,0 +1,28 @@
+const http = require("https");
+
+const options = {
+	"method": "GET",
+	"hostname": "burgers1.p.rapidapi.com",
+	"port": null,
+	"path": "/burgers",
+	"headers": {
+		"X-RapidAPI-Key": "5dbd4184e4mshd551a55bf8fd0f9p18eb3ajsnd48273def2fb",
+		"X-RapidAPI-Host": "burgers1.p.rapidapi.com",
+		"useQueryString": true
+	}
+};
+
+const req = http.request(options, function (res) {
+	const chunks = [];
+
+	res.on("data", function (chunk) {
+		chunks.push(chunk);
+	});
+
+	res.on("end", function () {
+		const body = Buffer.concat(chunks);
+		console.log(body.toString());
+	});
+});
+
+req.end();
